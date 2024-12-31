ff4554366fa73f4b2fda1487b1352d698e20ab9b
Asep Ready <savalunova@gmail.com>
2024-12-31 08:44:25 +0700

    worpress

--- root/bootstrap-wp.sh
+++ root/bootstrap-wp.sh
@@ -16,8 +16,39 @@ sed -i '' -e s#username_here#wpuser# \
 sed -i '' 's/localhost/127.0.0.1/' /usr/local/www/wordpress/wp-config.php
 
 ## import database
-echo "CREATE DATABASE wordpress; CREATE USER wpuser@localhost IDENTIFIED BY
-'$BOOTSTRAP_TOKEN'; GRANT ALL PRIVILEGES ON wordpress.* TO wpuser@localhost;" | mysql
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+mysql -u root -e "CREATE USER IF NOT EXISTS wpuser@localhost identified by '$BOOTSTRAP_TOKEN'"
+mysql -u root -e "GRANT ALL PRIVILEGES on wordpress.* to wpuser@localhost"
+mysql -u root -e "FLUSH PRIVILEGES"
+
+pkg install -y expect
+DB_ROOT_PASSWORD=$(openssl rand -base64 32) && export DB_ROOT_PASSWORD && echo $DB_ROOT_PASSWORD > /root/db_root_pwd.txt
+
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Switch to unix_socket authentication\"
+send \"n\r\"
+expect \"Change the root password?\"
+send \"n\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+
+echo "$SECURE_MYSQL"
+
+# Display the location of the generated root password for MySQL
+echo "Your DB_ROOT_PASSWORD is written on this file /root/db_root_pwd.txt"
+
+# No one but root can read this file. Read only permission.
+chmod 400 /root/db_root_pwd.txt
 
 ## cleanup
 rm /root/bootstrap-wp.sh
